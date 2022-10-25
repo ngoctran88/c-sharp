92498babd16906b47092b615a91ba9f139c78d8e
ngoctran88 <anhngocnt.88@gmail.com>
2022-10-26 00:35:33 +0700

    first commit

---
+++ Bai_15/Bai_15/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bai_15
+{
+    class Program
+    {
+        static void HocList()
+        {
+            //cấp phát bộ nhớ để sử dụng đối tượng list:
+            List<string> ds = new List<string>();
+            //thêm 1 phần tử vào danh sách:
+            ds.Add("An");
+            ds.Add("Bình");
+            ds.Add("Hạnh");
+            ds.Add("Phúc");
+            ds.Insert(2, "Ngọc");
+            ds.Remove("An");
+            ds.RemoveAt(0);
+
+            Console.WriteLine("Danh sách cac phần tử trong List là:");
+            for(int i=0;i<ds.Count;i++)
+            {
+                string v = ds[i];
+                Console.WriteLine(v);
+            }
+        }
+        static void HocList2()
+        {
+            List<int> ds = new List<int>();
+            Random rd = new Random();
+            for (int i = 0; i < 10; i++)
+                ds.Add(rd.Next(100));
+            Console.WriteLine("Các phần tử trong danh sách gốc là:");
+            foreach (int x in ds)
+                Console.Write(x + "\t");
+            ds.Sort();
+            Console.WriteLine("\nCác phần tử trong danh sách được sắp xếp là:");
+            foreach (int x in ds)
+                Console.Write(x + "\t");
+            int k;
+            Console.WriteLine("\nMời bạn nhập vào số muốn tìm:");
+
+            k = Convert.ToInt32(Console.ReadLine());
+            int kq = ds.IndexOf(k);
+            if (kq <0)
+                Console.WriteLine("Không tìm thấy {0} trong danh sách",k);
+            else
+                Console.WriteLine("Tìm thấy {0} tại vị trí {1}", k,kq);
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //HocList();
+            HocList2();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Bai_16/Bai_16/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bai_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            //Dictionary<string, double> dic2 = new Dictionary<string, double>();
+            dic.Add(1, "Nguyễn Văn Tèo");
+            dic.Add(2, "Trần Văn Tý");
+            if (dic.ContainsKey(2) == false)
+                dic.Add(2, "Đỗ Văn Ngố");
+            dic.Add(3, "Hoàng Văn Nam");
+            dic.Add(4, "Trần Anh Ngọc");
+            //để duyệt toàn bộ dữ liệu trong Dictionary ta làm như sau:
+            foreach (KeyValuePair<int,string> item in dic)
+            {
+                Console.WriteLine("Mã=" + item.Key + "; Tên=" + item.Value);
+            }
+            dic.Remove(2);
+            //để xóa dữ liệu trong Dictionary ta làm như sau:
+            Console.WriteLine("Sau khi xóa phần tử có khóa chính là 2:");
+            foreach (KeyValuePair<int, string> item in dic)
+            {
+                Console.WriteLine("Mã=" + item.Key + "; Tên=" + item.Value);
+            }
+
+            string value = dic[3];
+            Console.WriteLine("Đối tượng có khóa chính =3 là:" + value);
+            //trả về tập các giá trị:
+            List<string> dsGiaTri = dic.Values.ToList();
+            Console.WriteLine("Các giá trị là:");
+            foreach (string v in dsGiaTri)
+                Console.WriteLine(v);
+            //trả về khóa chính:
+            List<int> dsKey = dic.Keys.ToList();
+            Console.WriteLine("Các khóa chính là:");
+            foreach (int k in dsKey)
+                Console.WriteLine(k);
+            Console.ReadLine();
+        }
+    }
+}
